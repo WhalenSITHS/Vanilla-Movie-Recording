f5dd414adda42f058a42e8f33bdfb022a61bff93
mr. whalen <michael.whalen@sitechhs.com>
2021-04-15 07:32:02 -0400

    Done

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,47 @@ const key = "1fd276ec57b4baedacae00246e5cf4b7";
 ///////
 
 //////CODE GOES HERRR
+const query = async function () {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=6000&vote_average.gte=8&api_key=${key}`
+    );
+    const data = await response.json();
+    data.results.forEach((movie) => {
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `<div class="movie-card">
+      <div class="movie-card-front">
+        <img
+          src="https://image.tmdb.org/t/p/w300/${movie.poster_path}"
+          alt=""
+          class="poster"
+        />
+      </div>
+      <div class="movie-card-back">
+        <h3 class="movie-card-header">${movie.original_title}</h3>
+        <div class="score-box">
+          <p class="user-score">Community Score</p>
+          <p class="user-score">${movie.vote_average}</p>
+        </div>
+
+        <div class="release-box">
+          <p class="release-date">Released</p>
+          <p class="release-date">${movie.release_date}</p>
+        </div>
+
+        <div class="movie-genres">
+          <li class="movie-genre">Sci-Fi</li>
+          <li class="movie-genre">Fantasy</li>
+          <li class="movie-genre">Horror</li>
+        </div>
+      </div>
+    </div>`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+    alert("Hey something went wrong");
+  }
+};
+query();
